f0278a21ac02246877a8b217f9bc0750faf3b89a
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-06-27 16:11:18 -0400

    roboshop code new

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,5 @@
-dnf module list
-dnf moduel disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-cp catalogue.sevice /etc/systemd/system.catalogue.service
-systemctl enable catalogue
-systemctl start catalogue
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-systemctl daemon-reload
+source common.sh
+component = catalogue
+nodejs
 dnf install mongodb-mongosh -y
 mongosh --host 54.87.232.175 </app/db/master-data.js
