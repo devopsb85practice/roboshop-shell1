10f5970b9dffe41e62419bbac72fc46c0516dea0
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-06-27 22:03:33 -0400

    roboshop code new

--- catalogue.sh
+++ catalogue.sh
@@ -2,4 +2,4 @@ source common.sh
 component=catalogue
 nodejs
 dnf install mongodb-mongosh -y
-mongosh --host 54.87.232.175 </app/db/master-data.js
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
